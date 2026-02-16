c8ee200e18c21359b33a8a78cd7833602501820d
Dertko Daniil <64313607+dertkoD@users.noreply.github.com>
2026-02-16 13:07:44 +0200

    +

--- SofiasDreams/Assets/Scripts/Objects/GrapplePoint.cs
+++ SofiasDreams/Assets/Scripts/Objects/GrapplePoint.cs
@@ -7,9 +7,9 @@ public class GrapplePoint : MonoBehaviour
     public float gizmoSize = 0.15f;
     public Color gizmoColor = new Color(0.6f, 0.6f, 0.6f, 1f);
 
-    private void OnDrawGizmos()
+    /*private void OnDrawGizmos()
     {
         Gizmos.color = gizmoColor;
         Gizmos.DrawCube(transform.position, Vector3.one * gizmoSize);
-    }
+    }*/
 }
